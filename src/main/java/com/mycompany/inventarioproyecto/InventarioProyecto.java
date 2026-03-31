ff1aa690428cd0b45e4414b726cea789c205d2e5
Estefany <Estefany@192.168.1.9>
2026-03-30 21:36:45 -0600

    primer commit

---
+++ src/main/java/com/mycompany/inventarioproyecto/InventarioProyecto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventarioproyecto;
+
+/**
+ *
+ * @author Estefany
+ */
+public class InventarioProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Para inicar");
+
+    }
+}
